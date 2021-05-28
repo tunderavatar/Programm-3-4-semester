827c1e858f8d8925ccf18575636c70bfbc31ce35
tunderavatar <33571186+tunderavatar@users.noreply.github.com>
2021-05-28 16:38:27 +0300

    Create Buffer.h

---
+++ Buffer.h
@@ -0,0 +1,45 @@
+// Zavolokin Ilya, 211
+
+
+#pragma once
+
+#include <iostream>
+#include <sstream>
+
+#include "Point.h"
+#include "Field.h"
+#include "my_functions.h"
+
+class Field;
+class Point;
+class Cluster;
+
+using namespace std;
+
+#if !defined(BUFFER)
+#define BUFFER
+
+class Buffer
+{
+public:
+    explicit Buffer(Field* field);
+    ~Buffer();
+    void coutInfo(stringstream& message);
+    friend void operator+=(Buffer& left, Cluster& new_cl);
+    void putToField();
+    bool isEmpty();
+    friend class Field;
+private:
+    void writeLog(const string& message);
+    void rotatePoints(double alpha);
+    void movePoints(double x, double y);
+    void zoomPoints(double k);
+    void deletePoints();
+    void addCluster(Cluster& new_cl);
+    vector<Point> points;
+    ofstream& logs_f;
+    Field* p_field;
+    int id = 1;
+};
+
+#endif // BUFFER
